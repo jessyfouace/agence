8d84c9bc8c7a5681a6588cf5048c446932312097
jessyfouace <jessy.fouace62@gmail.com>
2018-12-26 15:59:48 +0100

    feat: frontend

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,26 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Twig\Environment;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\PropertyRepository;
+
+class HomeController extends AbstractController
+{
+    /**
+     *
+     * @Route("/", name="home")
+     * @param PropertyRepository $repository
+     * @return Response
+     *
+     */
+    public function index(PropertyRepository $repository): Response
+    {
+        $properties = $repository->findLaTest();
+        return $this->render('pages/home.html.twig', [
+            'properties' => $properties
+        ]);
+    }
+}
